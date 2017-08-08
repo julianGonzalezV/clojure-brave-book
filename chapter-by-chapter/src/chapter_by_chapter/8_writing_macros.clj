ee8e92eb9669ba178d67f94b642f1221a4de3991
julianGonzalezV <j.andres2087@gmail.com>
2017-08-07 22:44:01 -0500

    studying

--- chapter-by-chapter/src/chapter_by_chapter/8_writing_macros.clj
+++ chapter-by-chapter/src/chapter_by_chapter/8_writing_macros.clj
@@ -142,6 +142,164 @@
 
 
 ;:::::::::::::::::::Syntax Quoting:::::::::::::::::::::::::::
+; es la comilla inclinada `
+; note la diferencia como esta comilla muestra el namespace
+
+'+
+; => 
+
+'clojure.core/+
+; => clojure.core/+
+
+`+
+; => clojure.core/+
+
+'(+ 1 2)
+; => (+ 1 2)
+
+`(+ 1 2)
+; => (clojure.core/+ 1 2)
+
+;importante para evitar el name collisions,en donde si no le deciamos el namespace generaba error al momento de tener que se tuvieran dos funciones con el mismo nombre
+
+;Hacer uso de syntax quoting introduce la funcion de unquoting, eje
+
+
+`(+ 1 ~(inc 1))
+; => (clojure.core/+ 1 2)
 
 
 
+`(+ 1 (inc 1))
+; => (clojure.core/+ 1 (clojure.core/inc 1))
+
+; Es como una especie de String intrpolation, que nos permiten crear pantillas!! 
+
+(comment 
+
+name = "Jebediah"
+"Churn your butter, " + name + "!"
+
+
+;or through interpolation:
+
+"Churn your butter, #{name}!"
+)
+
+
+;note el vs de quoting and sintax quoting 
+
+(list '+ 1 (inc 1))
+; => (+ 1 2)
+
+; note como este es mas conciso
+`(+ 1 ~(inc 1))
+; => (clojure.core/+ 1 2)
+
+
+;Using Syntax Quoting in a Macro
+
+; SIN 
+
+(defmacro code-critic
+  "Phrases are courtesy Hermes Conrad from Futurama"
+  [bad good]
+  (list 'do
+        (list println
+              "Great squid of Madrid, this is bad code:"
+              (list 'quote bad))
+        (list println
+              "Sweet gorilla of Manila, this is good code:"
+              (list 'quote good))))
+
+;como bad and good no sosn symbols entonces se deben de anteponer con el la funcion quote
+
+(code-critic (1 + 1) (+ 1 1))
+;note en los mensajes que no se resuelve la suma
+; => Great squid of Madrid, this is bad code: (1 + 1)
+; => Sweet gorilla of Manila, this is good code: (+ 1 1)
+
+
+
+; CON SINTAX QUOTING
+;note que la suma no se resuelve
+;Se evita las repeticiones de List , sintactic sugar !
+(defmacro code-critic2
+  "Phrases are courtesy Hermes Conrad from Futurama"
+  [bad good]
+  `(do (println "Great squid of Madrid, this is bad code:"
+                (quote ~bad))
+       (println "Sweet gorilla of Manila, this is good code:"
+                (quote ~good))))
+
+(code-critic2 (1 + 1) (+ 1 1))
+; => Great squid of Madrid, this is bad code: (1 + 1)
+; => Sweet gorilla of Manila, this is good code: (+ 1 1)
+
+
+;Refactoring a Macro and Unquote Splicing
+
+
+;vamos a refactorizar code-critic
+
+;1) note como se repiten los ptinln en el code-critic2 (y 1 tambien)
+;entonces esas repeticiones se pueden sacar en una funcion aparte criticize-code
+;recibe la critica y el codigo y reotrna un sintax quoting que representa una lista, donde el primer elemento es la funcion println el segundo en criticism y el tercero el quote de code
+(defn criticize-code
+  [criticism code]
+  `(println ~criticism (quote ~code)))
+
+
+; el resultado es un codigo mas clareo :)
+(defmacro code-critic3
+  [bad good]
+  `(do ~(criticize-code "Cursed bacteria of Liberia, this is bad code:" bad)
+       ~(criticize-code "Sweet sacred boa of Western and Eastern Samoa, this is good code:" good)))
+
+
+(code-critic3 (1 + 1) (+ 1 1))
+
+; pero aun se puede mejorar mas!
+; podemos ver que de igual forma se sigue llamando una funcion(criticize-code) varias veces (2)
+
+;llamandolo con el map
+
+(map  #(apply criticize-code %) [["Cursed bacteria of Liberia, this is bad code:" 1]
+                                 ["Sweet gorilla of Manila, this is good code:" 1]])
+;((clojure.core/println "Cursed bacteria of Liberia, this is bad code:" (quote 1)) (clojure.core/println "Sweet gorilla of Manila, this is good code:" (quote 1)))
+
+;note como ya se llama una sola vez
+(defmacro code-critic4
+  [bad good]
+  `(do ~(map #(apply criticize-code %)
+             [["Great squid of Madrid, this is bad code:" bad]
+              ["Sweet gorilla of Manila, this is good code:" good]])))
+
+;sin embargo esto causa un NullPointer cual es el motivo ?
+; si vemos arriba aplicando el map el resultado es un list de prints y eso cuando se evalua es un nil
+(println "hola")
+;imprime en consola hola pero evalua a nil
+;volviendo al ejercicio entonces Do sobre nil genera Nu;; pointer
+(code-critic4 (1 + 1) (+ 1 1))
+
+
+;si solamente se aplica unquoting entonces:
+`(+ ~(list 1 2 3))
+; => (clojure.core/+ (1 2 3))
+
+
+
+; y si se aplica unquoting splicing, entonces elimina el sequence structure o wraper
+;y saca los datos dejandolos en el quoting que inicia el de antes de mas +
+`(+ ~@(list 1 2 3))
+; => (clojure.core/+ 1 2 3)
+
+;recordar que sin unquoting, lo que hace es no evaluar nada y retornar el namespace
+`(+ (list 1 2 3))
+;(clojure.core/+ (clojure.core/list 1 2 3))
+
+(defmacro code-critic5
+  [bad good]
+  `(do @~(map #(apply criticize-code %)
+              [["Great squid of Madrid, this is bad code:" bad]
+               ["Sweet gorilla of Manila, this is good code:" good]])))
